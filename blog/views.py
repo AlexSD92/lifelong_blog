effe635cb511247fd447866e800a78079e7c0c65
AlexSD92 <alex.spara.dominguez@gmail.com>
2025-07-04 14:34:48 +0100

    Add comment model, form, and approval workflow to post detail view

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post, Category
+from .forms import CommentForm
 
 def home(request):
     posts = Post.objects.filter(published=True).order_by('-created')
@@ -13,3 +14,25 @@ def category_posts(request, slug):
     category = get_object_or_404(Category, slug=slug)
     posts = Post.objects.filter(category=category, published=True)
     return render(request, 'blog/category.html', {'category': category, 'posts': posts})
+
+
+def post_detail(request, slug):
+    post = get_object_or_404(Post, slug=slug, published=True)
+    comments = post.comments.filter(approved=True)
+    new_comment = None
+
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            new_comment = form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()  # approved=False by default
+    else:
+        form = CommentForm()
+
+    return render(request, 'blog/post_detail.html', {
+        'post': post,
+        'comments': comments,
+        'form': form,
+        'new_comment': new_comment
+    })
