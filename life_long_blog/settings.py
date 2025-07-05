d53a2dd5365f249d18eb8c6342be91c9a7294543
AlexSD92 <alex.spara.dominguez@gmail.com>
2025-07-05 16:15:02 +0100

    Move DEFAULT_FROM_EMAIL config to .env using EMAIL_DISPLAY_NAME and formataddr()

--- life_long_blog/settings.py
+++ life_long_blog/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+from email.utils import formataddr
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -130,3 +134,11 @@ EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/accounts/login/"
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_DISPLAY_NAME = os.getenv("EMAIL_DISPLAY_NAME", "no-reply")
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
