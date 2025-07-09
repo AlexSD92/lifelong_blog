c0ae4c25ef02737d29118152ffd9e4b9f082e251
AlexSD92 <alex.spara.dominguez@gmail.com>
2025-07-09 14:37:17 +0100

    Add tagged_posts view, URL route, and template to display posts by tag

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('post/<slug:slug>/', views.post_detail, name='post_detail'),
     path('category/<slug:slug>/', views.category_posts, name='category_posts'),
+    path('tag/<slug:slug>/', views.tagged_posts, name='tagged_posts'),
     path('register/', views.register, name='register'), 
     path('delete-account/', views.delete_account, name='delete_account'),
     path('account-deleted/', TemplateView.as_view(template_name='blog/account_deleted.html'), name='account_deleted'),
